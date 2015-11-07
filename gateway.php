f86feb384f2abef84d36bc845280545e2dd2bcb8
Felipe Horta <fhorta@vidaserver1.poly.edu>
2015-11-07 15:45:25 -0500

    changing

--- gateway.php
+++ gateway.php
@@ -7,13 +7,13 @@ function cors() {
         header('Access-Control-Max-Age: 86400');    // cache for 1 day
     }
     // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: OPTIONS, HEAD, GET, POST, PUT, DELETE");
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-        exit(0);
-    }
+//     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+//             header("Access-Control-Allow-Methods: OPTIONS, HEAD, GET, POST, PUT, DELETE");
+//         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+//             header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+//         exit(0);
+//     }
 }
 function curl_get($url, $method, $params, array $get = NULL, array $options = array())
     {
